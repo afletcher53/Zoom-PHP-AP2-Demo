cb121f170f33304a4c5533e2d576d8a877588326
Aaron Fletcher <afletcher53@gmail.com>
2020-05-12 11:18:23 +0100

    initial set up

---
+++ app/Http/Controllers/ZoomAPIV2.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+use \Firebase\JWT\JWT;
+
+class ZoomAPIV2 extends Controller
+{
+
+
+    public function __construct(Request $request) {
+        //generate a 60 second JWT token on the fly
+        $api_secret = '';
+        $api_key = '';
+        $payload = array(
+        'iss' => $api_key,
+        'exp' => (time() + 60)
+        );
+        $this->jwt = JWT::encode($payload, $api_secret);
+
+        //create global guzzle client
+        $this->client = new \GuzzleHttp\Client(['base_uri' => 'http://api.zoom.us/v2/']);
+        $this->headers = [
+            'Authorization' => 'Bearer ' . $this->jwt,
+            'Accept'        => 'application/json',
+        ];
+    }
+
+    public function users() {
+        $response = $this->client->request('GET', 'users', [
+            'headers' => $this->headers
+        ]);
+        return $response;
+    }
+
+    public function listwebinars() {
+        $url = 'users/' . request()->route()->parameter('userId') . '/webinars';
+        $response = $this->client->request('GET', $url , [
+            'headers' => $this->headers
+        ]);
+        return $reponse;
+    }
+
+    public function getwebinars() {
+        $url = 'webinars/' . request()->route()->parameter('webinarId');
+        $response = $this->client->request('GET', $url , [
+            'headers' => $this->headers
+        ]);
+        return $response;
+    }
+
+    public function getwebinarparticipants() {
+        $url = 'metrics/webinars/' . request()->route()->parameter('webinarId') . '/participants';
+        $response = $this->client->request('GET', $url , [
+            'headers' => $this->headers
+        ]);
+        return $response;
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('ZoomAPI2/users', 'ZoomAPIV2@users');
+Route::get('ZoomAPI2/users/{userId}', 'ZoomAPIV2@listwebinars');
+Route::get('ZoomAPI2/webinars/{webinarId}', 'ZoomAPIV2@getwebinars');
+Route::get('ZoomAPI2/metrics/webinars/{webinarId}/participants', 'ZoomAPIV2@getwebinarparticipants');
